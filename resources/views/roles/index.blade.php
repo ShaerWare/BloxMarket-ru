a31be1a7b7ec2db0708c35ee3647996ce415ce0c
ShaerWare <artemsher@gmail.com>
2024-09-04 18:26:18 +0500

    roies index

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -4,11 +4,11 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Role Management</h2>
+            <h2>{{_('Настройка ролей пользователей')}}</h2>
         </div>
         <div class="pull-right">
         @can('role-create')
-            <a class="btn btn-success" href="{{ route('roles.create') }}"> создать новую роль</a>
+            <a class="btn btn-success" href="{{ route('roles.create') }}">{{_(' создать новую роль')}}</a>
             @endcan
         </div>
     </div>
@@ -22,18 +22,18 @@
 
 <table class="table table-bordered">
   <tr>
-     <th>No</th>
-     <th>Название</th>
-     <th width="280px">Действие (что может роль)</th>
+     <th>{{_('No')}}</th>
+     <th>{{_('Название')}}</th>
+     <th width="280px">{{_('Действие (что может роль)')}}</th>
   </tr>
     @foreach ($roles as $key => $role)
     <tr>
         <td>{{ ++$i }}</td>
         <td>{{ $role->name }}</td>
         <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Показать</a>
+            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">{{_('Показать')}}</a>
             @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Изменить</a>
+                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">{{_('Изменить')}}</a>
             @endcan
             @can('role-delete')
                 {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
