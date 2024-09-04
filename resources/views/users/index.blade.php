6ba6d723b0958bbb970f346a8ed690bb046ca2bb
ShaerWare <artemsher@gmail.com>
2024-09-05 02:50:04 +0500

    мультиязычность на продукт

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -45,7 +45,7 @@
                         @csrf
                         @method('DELETE')
                         <button
-                            class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition ml-4"
+                            class="btn btn-danger"
                             type="submit">{{ __('Удалить') }}</button>
                     </form>
                 </td>
