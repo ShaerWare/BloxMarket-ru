d1bd55c0ae6c4d6e7541803957af0d347e18d4c1
ShaerWare <artemsher@gmail.com>
2024-09-04 05:55:54 +0500

    круд ролей + круд товара

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
     Route::resource('/roles', RoleController::class);
     Route::resource('/users', UserController::class);
     Route::resource('/products', ProductController::class);
-    Route::get('/products', [ProductController::class, 'index'])->name('index');
+    //Route::get('/products', [ProductController::class, 'index'])->name('index');
 });
 
 require __DIR__.'/auth.php';
