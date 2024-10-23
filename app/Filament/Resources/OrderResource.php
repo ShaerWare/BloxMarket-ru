710075f077d8a352dfd4b5e909ed301d49ee91e8
vkn999 <volikovkn@gmail.com>
2024-10-23 18:10:18 +0300

    improve order filament

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -91,7 +91,7 @@ class OrderResource extends Resource
                     ->sortable()
                     ->summarize([
                         Tables\Columns\Summarizers\Sum::make()
-                            ->money(),
+                            ->money('RUB'),
                     ]),
                 Tables\Columns\TextColumn::make('status')
                     ->label('Статус')
